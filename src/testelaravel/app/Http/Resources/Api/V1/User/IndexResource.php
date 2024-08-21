6e06b3bdf6c0fc899e1909b75c70581f8c967269
J.S.Júnior <j.s.junior@live.com>
2024-08-20 21:13:18 -0300

    fix: removido resource antigo não usado

--- src/testelaravel/app/Http/Resources/Api/V1/User/IndexResource.php
+++ src/testelaravel/app/Http/Resources/Api/V1/User/IndexResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources\Api\V1\User;
 
-use App\Enums\Api\V1\Auth\RolesEnum;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,20 +13,11 @@ class IndexResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'type' => 'user',
             'id' => $this->ulid,
-            'attributes' => [
-                'name' => $this->full_name,
-                'suite' => $this->suite,
-                'email' => $this->email,
-                'phone' => $this->phone,
-                'isAdmin' => $this->hasRole(RolesEnum::ADMIN),
-                'emailVerifiedAt' => (bool) $this->email_verified_at,
-                'createdAt' => $this->created_at->format('Y-m-d H:i'),
-            ],
-            'links' => [
-                'self' => route('users.show', ['user' => $this->ulid]),
-            ],
+            'name' => $this->full_name,
+            'email' => $this->email,
+            'emailVerifiedAt' => (bool)$this->email_verified_at,
+            'createdAt' => $this->created_at->format('Y-m-d H:i'),
         ];
     }
 }
